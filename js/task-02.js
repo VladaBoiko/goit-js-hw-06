8a6c2b7ba5c07f02f47329ad51c39c428cad4a91
VladaBoiko <bermezha@gmail.com>
2022-07-20 17:41:13 +0300

    first ready

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,21 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+const parentList = document.querySelector("#ingredients");
+let createItemEl;
+const productList = (ingredients) => {
+  for (let i = 0; i <= ingredients.length - 1; i += 1) {
+    createItemEl = document.createElement("li");
+    createItemEl.classList.add(".item");
+    createItemEl.textContent = ingredients[i];
+    console.log(createItemEl);
+    parentList.append(createItemEl);
+  }
+};
+
+console.log(productList(ingredients));
