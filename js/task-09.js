b6105a9f1e0888d814748fe1616bae8d7ec4870f
VladaBoiko <bermezha@gmail.com>
2022-07-26 19:54:07 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+const body = document.querySelector("body");
+const button = body.querySelector(".change-color");
+const targetColor = body.querySelector(".color");
+
+button.addEventListener("click", changeColor);
+
+function changeColor() {
+  function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+  const color = getRandomHexColor();
+  targetColor.textContent = color;
+  body.style.backgroundColor = color;
 }
